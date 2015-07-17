066573a514b461f625c272b09da00280f5aeaeeb
manhnhhedspi <manhnhhedspi@gmail.com>
2015-07-17 15:33:51 +0700

    Finish remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == Settings.session.number ? remember(user) : forget(user)
       redirect_back_or user
     else
       flash.now[:danger] = t "controllers.sessions.create.flash_danger"
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to login_path
+    redirect_to root_path
   end
 end
