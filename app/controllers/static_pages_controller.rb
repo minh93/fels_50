405e9478c3700636745fa314aca3cdd798d21b4b
minh93 <phamducminh.bri@gmail.com>
2015-07-20 11:35:23 +0700

    finish show activities in home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @activities = current_user.activities.paginate page: params[:page],
+      per_page: Settings.activity.per_page
   end
 end
