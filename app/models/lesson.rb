ace80fd4d5ba8e99f2f508e275787a21c6c0663b
minh93 <phamducminh.bri@gmail.com>
2015-07-23 13:06:52 +0700

    finish user can answer question

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,6 +6,8 @@ class Lesson < ActiveRecord::Base
   has_many :words, through: :results
   has_many :answers, through: :results
 
+  accepts_nested_attributes_for :results
+
   before_create lambda {
     words = self.category.words.order("RANDOM()").limit 20
     self.words = words
