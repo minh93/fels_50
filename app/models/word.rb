471a1c158d98803b21c3562eafb4174389045111
minh93 <phamducminh.bri@gmail.com>
2015-07-28 09:33:42 +0700

    finished edit word

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,7 +5,11 @@ class Word < ActiveRecord::Base
   has_many :lessons, through: :results
 
   scope :learned, ->(user, category){joins(:lessons).where(lessons: {user_id: user.id})
-    .distinct.where.not(results: {answer_id: nil})}
-  scope :not_learn, ->(user, category){where.not(id: category.words.learned(user))}
+    .distinct.where.not results: {answer_id: nil}}
+  scope :not_learn, ->(user, category){where.not id: category.words.learned(user, category)}
   scope :all_word, ->(user, category){}
+
+  accepts_nested_attributes_for :answers,
+    reject_if: ->attributes{attributes["content"].blank?},
+    allow_destroy: true
 end
